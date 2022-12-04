23c0cd04112cbf1c80118bcac1d76565f20f57f2
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-12-04 17:54:34 -0500

    init

---
+++ README.md
@@ -0,0 +1,48 @@
+![Research Header Icons](./media/ResearchHeader.png)
+# Research Available
+Adds a beaker icon to any research tab which has blueprints that can be researched.  Excludes the blueprint currently being researched.
+# Installation 
+This mod requires the BepInEx mod loader.
+
+## BepInEx Setup
+If BepInEx has already been installed, skip this section.
+
+Download BepInEx from https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x64_5.4.21.0.zip
+
+* Extract the contents of the BepInEx zip file into the game's directory:
+```<Steam Directory>\steamapps\common\Card Survival Tropical Island```
+
+    __Important__:  The .zip file *must* be extracted to the root folder of the game.  If BepInEx was extracted correctly, the following directory will exist: ```<Steam Directory>\steamapps\common\Card Survival Tropical Island\BepInEx```.  This is a common install issue.
+
+* Run the game.  Once the main menu is shown, exit the game.
+    
+* In the BepInEx folder, there will now be a "plugins" directory.
+
+## Mod Setup
+* Download the ResearchAvailable.zip.  
+    * If on Nexumods.com, download from the Files tab.
+    * Otherwise, download from https://github.com/NBKRedSpy/CardSurvival-ResearchAvailable/releases/
+
+* Extract the contents of the zip file into the ```BepInEx/plugins``` folder.
+
+* Run the Game.  The mod will now be enabled.
+
+# Uninstalling
+
+## Uninstall
+This resets the game to an unmodded state.
+
+Delete the BepInEx folder from the game's directory
+```<Steam Directory>\steamapps\common\Card Survival Tropical Island\BepInEx```
+
+## Uninstalling This Mod Only
+
+This method removes this mod, but keeps the BepInEx mod loader and any other mods.
+
+Delete the ```ResearchAvailable``` folder from the ```<Steam Directory>\steamapps\common\Card Survival Tropical Island\BepInEx\plugins``` directory.
+# Compatibility
+Safe to add and remove from existing saves.
+
+# Acknowledgements
+
+[Beaker icon created by Dave Gandy - Flaticon](https://www.flaticon.com/free-icons/beaker)
---
+++ src/Patch_Patch.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ResearchAvailable
+{
+    [HarmonyPatch(typeof(BlueprintModelsScreen), nameof(BlueprintModelsScreen.Show))]
+    public static class BlueprintModelsScreen_ResearchIcon_Patch
+    {
+        /// <summary>
+        //  The Research Icon game objects.  They are a 1 to 1 index match with the game's tab groups.
+        /// </summary>
+        private static List<GameObject> ResearchIconObjects;
+
+        public static void Postfix(BlueprintModelsScreen __instance, List<IndexButton> ___TabButtons, GameManager ___GM)
+        {
+
+            if (ResearchIconObjects == null)
+            {
+                ResearchIconObjects = new List<GameObject>();
+
+                //Get the icon's file name
+                string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Texture2D texture = LoadPNG(Path.Combine(assemblyPath, "ResearchIcon.png"));
+
+                Sprite sprite = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), Vector2.zero);
+
+                for (int index = 0; index < ___TabButtons.Count; index++)
+                {
+                    GameObject gameObject = new GameObject();
+                    gameObject.name = "Available Research Icon";
+                    Image image = gameObject.AddComponent<Image>();
+                    image.sprite = sprite;
+
+                    gameObject.GetComponent<RectTransform>().SetParent(___TabButtons[index].transform);
+                    gameObject.transform.localScale = Vector3.one;
+                    gameObject.transform.position = Vector3.zero;
+                    RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+                    rectTransform.sizeDelta = new Vector2(texture.width, texture.height);
+                    //Don't know why, but this is the magic offset.  Zero is the center of the tab.
+                    rectTransform.localPosition = new Vector3(-64, 27, 0);
+
+                    ResearchIconObjects.Add(gameObject);
+                }
+            }
+
+            for (int i = 0; i < __instance.BlueprintTabs.Length; i++)
+            {
+                CardTabGroup group = __instance.BlueprintTabs[i];
+
+                ResearchIconObjects[i].SetActive(group != null && HasResearchable(__instance, ___GM, group));
+            }
+
+        }
+
+        /// <summary>
+        /// Returns true if the tab has at least one researchable card.  Excludes the card currently being researched.
+        /// </summary>
+        /// <param name="blueprintScreen"></param>
+        /// <param name="gameManager"></param>
+        /// <param name="tabGroup"></param>
+        /// <returns></returns>
+        private static bool HasResearchable(BlueprintModelsScreen blueprintScreen, GameManager gameManager, CardTabGroup tabGroup)
+        {
+            //Search all sub tabs for the current tab.
+            return tabGroup.SubGroups.Any(group => group.IncludedCards != null &&
+                group.IncludedCards.Any(card => card != null && blueprintScreen.CurrentResearch != card &&
+                    gameManager.PurchasableBlueprintCards.Contains(card)));
+        }
+
+        public static Texture2D LoadPNG(string filePath)
+        {
+            Texture2D tex = null;
+            byte[] fileData;
+
+            if (File.Exists(filePath))
+            {
+                fileData = File.ReadAllBytes(filePath);
+                tex = new Texture2D(2, 2);
+                tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+            }
+            return tex;
+        }
+    }
+}
